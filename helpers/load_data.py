99beec6dac713262a9dbd0c05c548b38b5085cda
Luke <lucusaurelius@gmail.com>
2019-04-18 23:28:53 -0400

    updated pre-processors

--- helpers/load_data.py
+++ helpers/load_data.py
@@ -2,12 +2,14 @@ import pandas as pd
 import numpy as np
 import os
 from multiprocessing import Pool
-from itertools import repeat
-
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy import event
+# from itertools import repeat
 
 # custom modules
 from lib.db import connect
 from lib.enums import ACQUISITION_RAW_COLUMN_NAMES, PERFORMANCE_RAW_COLUMN_NAMES
+from helpers.pre_process import PreProcessors, PRE_PROCESSING_ENCODERS_PICKLE_PATH
 
 
 def group(iterator, count):
@@ -59,7 +61,17 @@ def transform_columns(df, table):
     if 'acquisition' in table:
         set_cyclical_my('origination_date_string', df, 'origination')
         set_cyclical_my('first_payment_date_string', df, 'first_payment')
+
         df.relocation_mortgage_indicator = df.relocation_mortgage_indicator.eq('Y').mul(1)
+
+        int_colunns = ['loan_id', 'original_upb', 'original_loan_term',
+                       'original_loan_to_value', 'number_of_units', 'zip_code_short',
+                       'relocation_mortgage_indicator', 'origination_year',
+                       'first_payment_year']
+
+        pp.encode_numerical_columns(table, df[int_colunns].drop(['loan_id'], axis=1).astype('float64'))
+        # categories = df.select_dtypes(include='object')
+        # pp.encode_categorical_columns(table, [(name, categories[name]) for name in categories])
 
     elif 'performance' in table:
         set_cyclical_mdy('monthly_reporting_period', df, 'monthly_reporting')
@@ -72,42 +84,73 @@ def transform_columns(df, table):
         df.modification_flag = df.modification_flag.eq('Y').mul(1)
         df.repurchase_make_whole_proceeds_flag = df.repurchase_make_whole_proceeds_flag.eq('Y').mul(1)
         df.servicing_activity_indicator = df.servicing_activity_indicator.eq('Y').mul(1)
+
+        ints = df.select_dtypes(include=['int'])
+        print(ints.columns)
+        pp.encode_numerical_columns(table, ints.drop(['loan_id'], axis=1).astype('float64'))
+
     return df
 
 
 def to_sql(df, table):
-    # transform_columns(df, table)
-    df.to_sql(name=table, con=conn, index=False, if_exists='append')
+    try:
+        df.to_sql(name=table, con=conn, index=False, if_exists='append')
+    except IntegrityError:
+        print('rows already exist')
+        return 0
     return df.shape[0]
 
 
 def iterate_and_load(iterator, table=None):
     print('LOADING: {}'.format(table))
     count = 0
-    # sample = next(iterator)
-    # print(sample.sample(3))
-    # for df in iterator:
-    #     count += to_sql(df, table)
+    for df in iterator:
+        transform_columns(df, table)
+        count += df.shape[0]
+        break
+        # count += to_sql(df, table)
 
-    for dfs in group(iterator, 4):
-        # for r in pool.imap(transform_columns, zip(dfs, repeat(table))):
-        for df in pool.starmap(transform_columns,  zip(dfs, repeat(table))):
-            count += to_sql(df, table)
-
-            # count += r
-        print('LOADED: #{}'.format(count))
+        # for dfs in group(iterator, 4):
+        #     for df in pool.starmap(transform_columns, zip(dfs, repeat(table))):
+        # count += to_sql(df, table)
+        # count += df.shape[0]
+        # print('LOADED: #{}'.format(count))
     print('LOADED TOTAL: {}'.format(count))
 
 
 def main():
     for acq, perf in collect_raw_data_from_dirs():
-        # iterate_and_load(acq, 'acquisition')
+        iterate_and_load(acq, 'acquisition')
         iterate_and_load(perf, 'performance')
 
 
+
+# @event.listens_for(conn, 'before_cursor_execute')
+# def receive_before_cursor_execute(conn, cursor, statement, params, context, executemany):
+#     if executemany:
+#         cursor.fast_executemany = True
+
+
 if __name__ == '__main__':
+    conn = connect(local=True)
     pd.set_option('display.max_columns', None)  # or 1000
-    pool = Pool(processes=4)
-    conn = connect(local=True, pool_size=5)
+    # pool = Pool(processes=4)
+
+    pp = PreProcessors()
     main()
+    pp.save(PRE_PROCESSING_ENCODERS_PICKLE_PATH)
+
+    # test encoders
+    # a = load_pickle('../pickles/pre_processors.pkl')
+    # p = load_encoder('../pickles/performance_encoder.pkl')
+
+    pp = PreProcessors()
+    pp.load(PRE_PROCESSING_ENCODERS_PICKLE_PATH)
+    print(dir(pp))
+    print(pp.encoders)
+    # print(a.original_loan_to_value.mean_)
+    #
+    # print(dir(p))
+    # print(p.loan_age.mean_)
+
     # conn.close()
--- helpers/pre_process.py
+++ helpers/pre_process.py
@@ -1,99 +1,160 @@
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 import numpy as np
 import pickle
+from os import listdir
+
 from lib.db import connect
 
+PRE_PROCESSING_ENCODERS_PICKLE_PATH = '../pickles/pre_processing_encoders.pkl'
 
-def load_encoder(table):
-    with open("{}_encoder.pkl".format(table), "rb") as infile:
+
+def load_pickle(path):
+    with open(path, "rb") as infile:
         loaded = pickle.load(infile)
     return loaded
 
 
-def create_encoding(table, column_names):
-    class Encoding:
-        __slots__ = ('table', *column_names)
+class ActiveEncoder(object):
+    __slots__ = ('encoder', 'columns')
 
-        def __init__(self, table=None):
-            self.table = table
+    def __init__(self, encoder=None, columns=None):
+        self.encoder = encoder
+        self.columns = columns
 
-        def categorical(self, name, categories):
+
+class Encoding:
+    def __init__(self, table=None):
+        self.table = table
+
+    def categorical(self, name, categories):
+        if not hasattr(self, name):
             ohe = OneHotEncoder(sparse=False)
-            ohe.fit(categories)
             setattr(self, name, ohe)
-            print('OneHotEncoder', ohe.categories_)
+        else:
+            ohe = getattr(self, name)
+        try:
+            ohe.fit(categories)
+            print('OneHotEncoder:', name, ohe.categories_)
             return ohe
+        except Exception as ex:
+            print(ex)
 
-        def integer(self, name, categories):
+    def numerical(self, name, items):
+        if not hasattr(self, name):
             ss = StandardScaler()
-            ss.fit(categories)
             setattr(self, name, ss)
-            print('StandardScaler', ss.mean_)
-            return ss
+        else:
+            ss = getattr(self, name)
 
-        def apply_encoding(self, name, categories):
-            if not hasattr(self, name):
-                raise Exception(
-                    'You did not create an encoder for {} categories, use the categorical() method first'.format(name))
-            return self[name].transform(categories)
+        ss.partial_fit(items)
+        print('StandardScaler:', name, ss.mean_, ss.var_)
+        return ss
 
-        def save(self):
-            with open("{}_encoder.pkl".format(self.table), "wb") as outfile:
-                pickled = pickle.dump(self, outfile)
-            print("pickled: {} encoders".format(self.table))
-            return pickled
+    def apply_encoding(self, name, categories):
+        if not hasattr(self, name):
+            raise Exception(
+                'You did not create an encoder for {} categories, use the categorical() method first'.format(name))
+        return self[name].transform(categories)
 
-    return Encoding(table)
+    def save(self, path=None):
+        name = "{}/{}_encoder.pkl".format(path or '.', self.table)
+        with open(name, "wb") as outfile:
+            pickled = pickle.dump(self, outfile)
+        print("pickled: {} encoders @ {}".format(self.table, name))
+        return pickled
 
-
-def get_column_by_type(table, type, conn):
-    cur = conn.execute("""
-       SELECT column_name
-       FROM information_schema.columns
-       WHERE table_name = '{}'
-       AND data_type = '{}'
-       """.format(table, type)
-                       )
-    for name, *others in cur:
-        c = conn.execute("SELECT DISTINCT({0}) FROM {1} ORDER BY {0} ASC".format(name, table))
-        categories = list(c)
-        # print(name, categories)
-        yield name, np.asarray(categories)
+    def load(self, path=None):
+        name = "{}/{}_encoder.pkl".format(path or '.', self.table)
+        return load_pickle(name)
 
 
-def encode_categorical_columns(table, iterator):
-    items = list(iterator)
-    encode = create_encoding(table, list(map(lambda n: n[0], items)))
-    for name, col in items:
-        encode.categorical(name, col)
-    return encode
+class PreProcessors:
+    __slots__ = ('_conn', 'encoders', '_excluded_columns')
 
+    def __init__(self, conn=None, excluded_columns=None):
+        self._conn = conn
+        self.encoders = dict()
+        self._excluded_columns = excluded_columns or []
 
-def encode_integer_columns(table, iterator):
-    items = list(iterator)
-    encode = create_encoding(table, list(map(lambda n: n[0], items)))
-    for name, col in items:
-        encode.integer(name, np.array(col, dtype=int))
-    return encode
+    def get_column_by_type(self, table, type, names_only=False):
+        cur = conn.execute("""
+           SELECT column_name
+           FROM information_schema.columns
+           WHERE table_name = '{}'
+           AND data_type = '{}'
+           """.format(table, type)
+                           )
+        if names_only:
+            return cur
 
+        for name, *others in cur:
+            if name not in self._excluded_columns:
+                c = self._conn.execute(
+                    "SELECT DISTINCT({0}) FROM {1} ORDER BY {0} ASC".format(name, table))
+                categories = list(map(lambda x: x if x[0] else ['X'], c))
+                yield name, np.asarray(categories)
 
-def encode_table(table, conn):
-    # char_columns = get_column_by_type(table, 'character varying', conn)
-    # char_encoder = encode_categorical_columns(table, char_columns)
-    int_columns = get_column_by_type(table, 'integer', conn)
-    int_encoder = encode_integer_columns(table, int_columns)
-    # encoders = dict(char_encoder=char_encoder, int_encoder=int_encoder)
-    encoders = dict(char_encoder='', int_encoder=int_encoder)
+    def encode_categorical_columns(self, table, iterator=None):
+        items = iterator or self.get_column_by_type(table, 'character varying')
+        encode = self.encoders.get('{}_categorical'.format(table), ActiveEncoder()).encoder or Encoding(table)
+        names = []
+        for name, col in items:
+            encode.categorical(name, col)
+            names.append(name)
+        self.encoders['{}_categorical'.format(table)] = ActiveEncoder(encoder=encode, columns=names)
+        return encode
 
-    # for k, encoder in encoders.items():
-    #     print(k, [e for e in dir(encoder) if not e.startswith('__')])
-    return encoders
+    def encode_numerical_columns(self, table, df):
+        encode = self.encoders.get('{}_numerical'.format(table), ActiveEncoder()).encoder or Encoding(table)
+        encode.numerical('standard', df)
+        self.encoders['{}_numerical'.format(table)] = ActiveEncoder(encoder=encode, columns=df.columns.tolist())
+        return encode
+
+    def save(self, path=None):
+        del self._conn
+        p = path or 'pre_processing_encoders'
+        name = "{}.pkl".format(p) if not p.endswith('.pkl') else p
+        with open(name, "wb") as outfile:
+            pickled = pickle.dump(self, outfile)
+        print("pickled: {} encoders @ {}".format(len(self.encoders), name))
+        return pickled
+
+    def load(self, path):
+        pp = load_pickle(path)
+        if pp and hasattr(pp, 'encoders'):
+            print('Loading pre-processors from {}, adding {} encoders'.format(path, len(pp.encoders)))
+            self.encoders = pp.encoders
+
+    def save_individual(self, path=None):
+        for name, encoder_dict in self.encoders.items():
+            encoder_dict.get('encoder').save(path)
+
+    def load_individual(self, path):
+        pickles = listdir(path)
+        for file in pickles:
+            encoder = load_pickle('{}/{}'.format(path, file))
+            name = file.replace("_encoder.pkl", '')
+            self.encoders[name] = encoder
 
 
 if __name__ == '__main__':
-    conn = connect(local=True).connect()
-    acq_encoders = encode_table('acquisition', conn)
-    # perf_encoders = encode_table('performance', conn)
+    conn = connect(local=True)
 
-    # e = load_encoder('acquisition')
-    # print(dir(e))
+    EXCLUDED = [
+        'origination_date_string', 'first_payment_date_string',
+        'monthly_reporting_period', 'maturity_date_string',
+        'zero_balance_effective_date_string', 'last_paid_installment_date_string',
+        'disposition_date_string', 'foreclosure_date_string', 'product_type'
+    ]
+
+    pp = PreProcessors(conn=conn, excluded_columns=EXCLUDED)
+    pp.load(PRE_PROCESSING_ENCODERS_PICKLE_PATH)
+    pp.encode_categorical_columns('acquisition')
+    pp.encode_categorical_columns('performance')
+
+    pp.save(PRE_PROCESSING_ENCODERS_PICKLE_PATH)
+
+    test = PreProcessors()
+    test.load(PRE_PROCESSING_ENCODERS_PICKLE_PATH)
+    print(dir(test))
+    print(test.encoders)
